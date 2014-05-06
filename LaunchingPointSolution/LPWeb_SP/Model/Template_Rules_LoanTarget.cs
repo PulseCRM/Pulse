9f485329bf4ffc53a97aedea4ebc2b73c2eb4c28
focusitinc <josh.bopp@focusitinc.com>
2014-05-06 19:31:04 +0530

    Loan targets shown on Rule List page
    Loan targets shown on Rule List page for issue # 203.

--- LaunchingPointSolution/LPWeb_SP/Model/Template_Rules_LoanTarget.cs
+++ LaunchingPointSolution/LPWeb_SP/Model/Template_Rules_LoanTarget.cs
@@ -7,27 +7,27 @@ namespace LPWeb.Model
         public Template_Rules_LoanTarget() { }
         public Template_Rules_LoanTarget(Int16 iLoanTargetValue)
         {
-            if (iLoanTargetValue == this._constOldProcessing) { this.ActiveLoans = true; }
-            else if (iLoanTargetValue == this._constOldProspect) { this.ActiveLeads = true; }
-            else if (iLoanTargetValue == this._constOldProcessingAndProspect) { this.ActiveLoans = this.ActiveLeads = true; }
+            if (iLoanTargetValue == _constOldProcessing) { this.ActiveLoans = true; }
+            else if (iLoanTargetValue == _constOldProspect) { this.ActiveLeads = true; }
+            else if (iLoanTargetValue == _constOldProcessingAndProspect) { this.ActiveLoans = this.ActiveLeads = true; }
             else
             {
-                if ((iLoanTargetValue & this._constActiveLoans) == this._constActiveLoans) { this.ActiveLoans = true; }
-                if ((iLoanTargetValue & this._constActiveLeads) == this._constActiveLeads) { this.ActiveLeads = true; }
-                if ((iLoanTargetValue & this._constArchivedLoans) == this._constArchivedLoans) { this.ArchivedLoans = true; }
-                if ((iLoanTargetValue & this._constArchivedLeads) == this._constArchivedLeads) { this.ArchivedLeads = true; }
+                if ((iLoanTargetValue & _constActiveLoans) == _constActiveLoans) { this.ActiveLoans = true; }
+                if ((iLoanTargetValue & _constActiveLeads) == _constActiveLeads) { this.ActiveLeads = true; }
+                if ((iLoanTargetValue & _constArchivedLoans) == _constArchivedLoans) { this.ArchivedLoans = true; }
+                if ((iLoanTargetValue & _constArchivedLeads) == _constArchivedLeads) { this.ArchivedLeads = true; }
             }
         }
 
         #region Model
 
-        private readonly Int16 _constOldProcessing = 0;
-        private readonly Int16 _constOldProspect = 1;
-        private readonly Int16 _constOldProcessingAndProspect = 2;
-        private readonly Int16 _constActiveLoans = Convert.ToInt16("10001", 2);
-        private readonly Int16 _constActiveLeads = Convert.ToInt16("10010", 2);
-        private readonly Int16 _constArchivedLoans = Convert.ToInt16("10100", 2);
-        private readonly Int16 _constArchivedLeads = Convert.ToInt16("11000", 2);
+        private static readonly Int16 _constOldProcessing = 0;
+        private static readonly Int16 _constOldProspect = 1;
+        private static readonly Int16 _constOldProcessingAndProspect = 2;
+        private static readonly Int16 _constActiveLoans = Convert.ToInt16("10001", 2);
+        private static readonly Int16 _constActiveLeads = Convert.ToInt16("10010", 2);
+        private static readonly Int16 _constArchivedLoans = Convert.ToInt16("10100", 2);
+        private static readonly Int16 _constArchivedLeads = Convert.ToInt16("11000", 2);
 
         public bool ActiveLoans { get; set; }
         public bool ActiveLeads { get; set; }
@@ -40,10 +40,10 @@ namespace LPWeb.Model
             {
                 Int16 returnValue = 0;
 
-                if (this.ActiveLoans) { returnValue = (Int16)(returnValue | this._constActiveLoans); }
-                if (this.ActiveLeads) { returnValue = (Int16)(returnValue | this._constActiveLeads); }
-                if (this.ArchivedLoans) { returnValue = (Int16)(returnValue | this._constArchivedLoans); }
-                if (this.ArchivedLeads) { returnValue = (Int16)(returnValue | this._constArchivedLeads); }
+                if (this.ActiveLoans) { returnValue = (Int16)(returnValue | _constActiveLoans); }
+                if (this.ActiveLeads) { returnValue = (Int16)(returnValue | _constActiveLeads); }
+                if (this.ArchivedLoans) { returnValue = (Int16)(returnValue | _constArchivedLoans); }
+                if (this.ArchivedLeads) { returnValue = (Int16)(returnValue | _constArchivedLeads); }
 
                 return returnValue;
             }
